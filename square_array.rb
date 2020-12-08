4000da750081858dfb8420551e059c9a37e9dc28
Rick <dirklo@gmail.com>
2020-12-08 09:57:43 +0100

    commit tree fbbfd419afd81e466b6251c9af6d537dce6ebc26

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,17 @@
 def square_array(array)
-  # your code here
+  final = []
+  array.each do |num|
+    final.push(num**2)
+  end
+  return final  
 end
+
+
+
+# array = [1, 2, 3]
+
+# def square_array(array)
+#   array.collect {|num| num**2}
+# end
+
+# puts square_array(array)
